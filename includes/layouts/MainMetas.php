99f3b3aa066e5d7c335472fd49a0d3f6ce100333
Enriko Todorov <impossiblelimner@gmail.com>
2021-01-05 01:40:11 +0000

    Промени по кода

--- includes/layouts/MainMetas.php
+++ includes/layouts/MainMetas.php
@@ -1,6 +1,6 @@
-<link rel="canonical" href="https://getbootstrap.com/">
-<meta name="description" content="The most popular HTML, CSS, and JS library in the world.">
-<meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+
+<meta name="description" content="Реклама на игрови сървъри">
+<meta name="author" content="hybridmind">
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" crossorigin="anonymous">
